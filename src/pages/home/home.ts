d360f5e2c75cc4b57351cf3e27239887b75b8fcb
lffelizardo <lfernandes.felizardo@gmail.com>
2018-05-02 19:57:36 -0300

    getting data from form

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { MenuController } from 'ionic-angular/components/app/menu-controller'
+import { CredenciaisDTO } from '../../models/credenciais.dto';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  creds: CredenciaisDTO = {
+    email: "",
+    senha: ""
+  }
 
   constructor(public navCtrl: NavController, public menu: MenuController) {
 
