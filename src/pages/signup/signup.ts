d4ba109de5e6dfb9a39cb850de3825e8b023524c
lffelizardo <lfernandes.felizardo@gmail.com>
2018-05-15 17:20:03 -0300

    creating form group

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 /**
  * Generated class for the SignupPage page.
@@ -15,7 +16,31 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SignupPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  formGroup: FormGroup;
+
+  numberPattern = /^[0-9]*$/;
+
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public formBuilder: FormBuilder) {
+
+                this.formGroup = this.formBuilder.group({
+                  nome: ['',[Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
+                  email: ['', [Validators.required, Validators.email]],
+                  tipo: ['', [Validators.required]],
+                  cpfOuCnpj: ['', [Validators.required, Validators.minLength(11), Validators.pattern(this.numberPattern)]],
+                  senha: ['', [Validators.required]],
+                  logradouro: ['', [Validators.required]],
+                  numero: ['', [Validators.required, Validators.pattern(this.numberPattern)]],
+                  complemento: ['', []],
+                  bairro: ['', Validators.required],
+                  cep: ['', [Validators.required]],
+                  telefone1: ['', [Validators.required]],
+                  telefone2: ['', [Validators.required]],
+                  telefone3: ['', [Validators.required]],
+                  estadoId: [null, Validators.required],
+                  cidadeId: [null, Validators.required]
+                });
   }
 
   ionViewDidLoad() {
