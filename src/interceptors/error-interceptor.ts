7a9a669332b11274fbd9d7bd338a06944c153fad
lffelizardo <lfernandes.felizardo@gmail.com>
2018-05-18 18:59:03 -0300

    building product page

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -3,6 +3,7 @@ import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent, HTTP_INTERCEPTORS
 import { Observable } from "rxjs/Rx";
 import { StorageService } from "../services/storage.service";
 import { AlertController } from "ionic-angular";
+import { FieldMessage } from "../models/fieldmessage";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
@@ -21,8 +22,6 @@ export class ErrorInterceptor implements HttpInterceptor {
             if(!errorObj.status){
                 errorObj = JSON.parse(errorObj);
             }
-            console.log("Error detectado pelo interceptor");
-            console.log(errorObj);
 
             switch(errorObj.status){
                 case 401:
@@ -30,6 +29,9 @@ export class ErrorInterceptor implements HttpInterceptor {
                 break;
                 case 403:
                     this.handle403();
+                break;
+                case 422:
+                    this.handle422(errorObj);
                 break;
                 default:
                     this.handleDefaultError(errorObj);
@@ -55,7 +57,31 @@ export class ErrorInterceptor implements HttpInterceptor {
         this.storage.setLocalUser(null);
     }
 
+    handle422(errorObj){
+        console.log('#########');
+        console.log(this.listErrors(errorObj.errors));
+        let alert = this.alert.create({
+            title: 'Erro de validação',
+            message: this.listErrors(errorObj.errors),
+            enableBackdropDismiss: false,
+            buttons: [
+                {text: 'Ok'}
+            ]
+        });
+        
+        alert.present();
+    }
+
+    private listErrors(messages: FieldMessage[]): string{
+        let s: string = '';
+        for(var i=0; i<messages.length; i++){
+            s = s + '<p><strong>' + messages[i].fieldName + '</strong>: ' + messages[i].message + '</p>'; 
+        }
+        return s;
+    }
+
     handleDefaultError(errorObj){
+        console.log('caiu aki')
         let alert = this.alert.create({
             title: `Erro ${errorObj.status} : ${errorObj.error}`,
             message: errorObj.message,
--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -22,4 +22,13 @@ export class ClienteService {
         let url = `${API_CONFIG.bucketBaseUrl}/cp${id}.jpg`;
         return this.http.get(url, {responseType: 'blob'})
     }
+
+    insert(obj: ClienteDTO): Observable<any>{
+        console.log(obj);
+        return this.http.post(`${API_CONFIG.baseUrl}/clientes`, obj,
+        {
+            observe: 'response',
+            responseType: 'text'
+        });
+    }
 }
