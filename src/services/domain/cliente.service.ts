7d2ae05a3fa80c1bbfd754075e4af885107e66e9
lffelizardo <lfernandes.felizardo@gmail.com>
2018-06-02 13:58:49 -0300

    developing order confirmation

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -31,4 +31,8 @@ export class ClienteService {
             responseType: 'text'
         });
     }
+
+    findById(id:string){
+        return this.http.get(`${API_CONFIG.baseUrl}/clientes/${id}`);
+    }
 }
